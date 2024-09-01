2ba1432709fab12af036f61cb6ebe023f997c1d5
Husky <husky.robot.dog@gmail.com>
2024-09-01 22:51:58 +0800

    1

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { RunMode, BacktestConfig, Decimal, runBacktest, Context } from "./wealth";
+
+function onSetup(ctx: Context) {
+  ctx.setBenchmark("BTCUSDT");
+}
+
+const config = new BacktestConfig().begin("2021-01-01").end("2021-01-05").balance(Decimal.fromString("1000"));
+runBacktest(config, {
+  onSetup
+});
