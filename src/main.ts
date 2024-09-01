cc345d0a6d9ef005c1f028e719f36c61d5fd4572
Husky <husky.robot.dog@gmail.com>
2024-09-02 00:46:10 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { BacktestConfig, Decimal, runBacktest, Context } from "./wealth";
+import moment from "moment";
 
 /** 设置 */
 function onSetup(ctx: Context) {
@@ -16,16 +17,34 @@ function onMinuteBegin(ctx: Context) {}
 function onTick(ctx: Context) {}
 /** 每分钟结束 */
 function onMinuteEnd(ctx: Context) {
-  const tradeTime = ctx.tradeTime();
+  if (1 == 1) return;
+  // K线
   const candle = ctx.lastCandle("BTCUSDT");
   if (!candle) return;
-  ctx.info(`时间(${tradeTime.toLocaleString()}), 收盘价(${candle.close.toString()})`);
+  // 指数价
+  const indexPrice = ctx.indexPrice("BTCUSDT");
+  if (!indexPrice) return;
+  // 标记价
+  const markPrice = ctx.markPrice("BTCUSDT");
+  if (!markPrice) return;
+  // 资金费率
+  const fundingRate = ctx.fundingRate("BTCUSDT");
+  if (!fundingRate) return;
+
+  const tradeTime = ctx.tradeTime();
+  const tradeTimeStr = moment(tradeTime).format("YYYY-MM-DD HH:mm:ss");
+  ctx.info(
+    `时间(${tradeTimeStr}), 收盘价(${candle.close.toString()}), 指数价(${indexPrice.toString()}), 标记价(${markPrice.toString()}), 资金费率(${fundingRate.toString()})`
+  );
 }
 /** 每小时结束 */
 function onHourEnd(ctx: Context) {}
 /** 每日结束 */
 function onDayEnd(ctx: Context) {
-  // ctx.debug("每日结束");
+  const account = ctx.account();
+  ctx.info(
+    `账户: 余额(${account.balance.toString()}), 可用余额(${account.available.toString()}), 未实现盈亏(${account.upl.toString()})`
+  );
 }
 /** 策略停止 */
 function onStop(ctx: Context) {
